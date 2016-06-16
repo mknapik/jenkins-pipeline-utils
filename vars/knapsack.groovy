6c48703ec4a85789b5b22a263d01155ae6fc697b
Michał Knapik <mknapik@users.noreply.github.com>
2016-06-16 11:47:56 +0200

    Knapsack allows to label nodes

--- vars/knapsack.groovy
+++ vars/knapsack.groovy
@@ -1,10 +1,10 @@
-def call(String step_name, int ci_node_total, Closure cl) {
+def call(String step_name, int ci_node_total, String label = null, Closure cl) {
   def nodes = [:]
 
   for(int i = 0; i < ci_node_total; i++) {
     def index = i;
     nodes["${step_name}_ci_node_${i}"] = {
-      node {
+      node(label) {
         withEnv(["CI_NODE_INDEX=$index", "CI_NODE_TOTAL=$ci_node_total"]) {
           cl()
         }
