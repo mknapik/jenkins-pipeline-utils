dfd32e893647739d2b1e7d6b0775ebc7412ab25e
Michał Knapik <mknapik@users.noreply.github.com>
2016-06-16 16:37:21 +0200

    Update knapsack.groovy

--- vars/knapsack.groovy
+++ vars/knapsack.groovy
@@ -2,7 +2,7 @@ def call(String step_name, int ci_node_total, Closure cl) {
   call(step_name, ci_node_total, null, cl)
 }
 
-def call(String step_name, int ci_node_total, String label = null, Closure cl) {
+def call(String step_name, int ci_node_total, String label, Closure cl) {
   def nodes = [:]
 
   for(int i = 0; i < ci_node_total; i++) {
