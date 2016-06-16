facb9e1fac880ebf797b4f9359b829009da1e89f
Michał Knapik <michal.knapik@u2i.com>
2016-06-16 18:25:17 +0200

    withMysql refactoring

--- vars/withMysql.groovy
+++ vars/withMysql.groovy
@@ -1,6 +1,6 @@
 def call(String credential_id, Closure cl) {
     withCredentials(
-        [[$class: 'UsernamePasswordMultiBinding', credentialsId: "${credential_id}",
+        [[$class: 'UsernamePasswordMultiBinding', credentialsId: credential_id,
             usernameVariable: 'MYSQL_USERNAME', passwordVariable: 'MYSQL_PASSWORD']]
     ) {
         cl()
