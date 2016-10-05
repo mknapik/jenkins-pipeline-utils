5c54f7effe036b7d189e9e08ffb9f2d78bee8e11
Michał Knapik <mknapik@users.noreply.github.com>
2016-10-05 12:37:12 +0200

    Update discardOldBuilds.groovy
    Don't pass artifact options

--- vars/discardOldBuilds.groovy
+++ vars/discardOldBuilds.groovy
@@ -1,7 +1,8 @@
 def call(Map<String, Integer> args) {
     def daysToKeepStr = "${args.days ?: 100}"
     def numToKeepStr = "${args.num ?: 30}"
-    def strategy = [$class: 'LogRotator', artifactDaysToKeepStr: '0', artifactNumToKeepStr: '0', daysToKeepStr: daysToKeepStr, numToKeepStr: numToKeepStr]
+    // artifactDaysToKeepStr: '0', artifactNumToKeepStr: '0', 
+    def strategy = [$class: 'LogRotator', daysToKeepStr: daysToKeepStr, numToKeepStr: numToKeepStr]
     def property = [$class: 'BuildDiscarderProperty', strategy: strategy]
     properties([property])
 }
