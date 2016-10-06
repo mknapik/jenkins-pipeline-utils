d3529a95b120cd47aa373f507ba3555c73b387d7
Michał Knapik <mknapik@users.noreply.github.com>
2016-10-06 13:07:57 +0200

    Set DOCKER_IMAGE_TAG env var

--- vars/setDockerImageName.groovy
+++ vars/setDockerImageName.groovy
@@ -1,5 +1,14 @@
 def call() {
-    env.DOCKER_IMAGE_NAME = replaceUnallowedCharacters(env.JOB_NAME)
+    if(env.BRANCH_NAME == null) {
+        env.DOCKER_IMAGE_TAG = 'latest'
+        env.DOCKER_IMAGE_NAME = replaceUnallowedCharacters(env.JOB_NAME)
+    } else {
+        def tag = replaceUnallowedCharacters(env.BRANCH_NAME)
+        def job_name = replaceUnallowedCharacters(env.JOB_NAME)
+        def image_name = job_name.replaceAll(/.${tag}^/, '')
+        env.DOCKER_IMAGE_TAG = tag
+        env.DOCKER_IMAGE_NAME = image_name
+    }
 }
 
 def replaceUnallowedCharacters(String string) {
