4c742fb8bb34fc2948bc0e3cfde7b6b99bc7d02a
Michał Knapik <michal.knapik@u2i.com>
2016-10-11 11:24:54 +0200

    Implement foreachPullRequest

---
+++ src/me/knapik/GitHub.groovy
@@ -0,0 +1,31 @@
+package me.knapik
+
+import org.jenkinsci.plugins.workflow.cps.CpsScript
+import com.cloudbees.groovy.cps.NonCPS
+
+class GitHub implements Serializable {
+    @NonCPS
+    static def findPullRequests(String jobName, String branchName, String githubAccessToken) {
+        def (String owner, String repo) = jobName.split('/')
+        String head = "${owner}:${branchName}"
+
+        String result = new URL("https://api.github.com/repos/${owner}/${repo}/pulls?access_token=${githubAccessToken}&state=open&head=${head}").
+            getText(connectTimeout: 5000,
+                     readTimeout: 10000,
+                     useCaches: true,
+                     allowUserInteraction: false,
+                     requestProperties: ['Connection': 'close'])
+
+        def json = jsonParse(result)
+
+        List<Integer> pullRequestIds = []
+        List<String> targetBranchRefs = []
+
+        for(int i = 0; i < json.size; ++i) {
+            pullRequestIds << json[i]['number']
+            targetBranchRefs << json[i]['base']['ref']
+        }
+
+        return [pullRequestIds: pullRequestIds, targetBranchRefs: targetBranchRefs]
+    }
+}
---
+++ vars/foreachPullRequest.groovy
@@ -0,0 +1,12 @@
+def call(String credentialsId, Closure cl) {
+    withCredentials([[$class: 'StringBinding', credentialsId: credentialsId, variable: 'GITHUB_ACCESS_TOKEN']]) {
+        def result = me.knapik.GitHub.findPullRequests(env.JOB_NAME, env.BRANCH_NAME, env.GITHUB_ACCESS_TOKEN)
+
+        def pullRequestIds = result['pullRequestIds']
+        def targetBranchRefs = result['targetBranchRefs']
+
+        for (int i = 0; i < pullRequestIds.size(); ++i) {
+            cl(pullRequestIds[i], targetBranchRefs[i])
+        }
+    }
+}
