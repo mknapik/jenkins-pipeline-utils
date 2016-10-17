06fd6ddebd93db93c1f905e1481d7128436ac019
Michał Knapik <mknapik@users.noreply.github.com>
2016-10-17 10:11:55 +0200

    Pass GitHub token to foreachPullRequest closure

--- vars/foreachPullRequest.groovy
+++ vars/foreachPullRequest.groovy
@@ -1,12 +1,13 @@
 def call(String credentialsId, Closure cl) {
     withCredentials([[$class: 'StringBinding', credentialsId: credentialsId, variable: 'GITHUB_ACCESS_TOKEN']]) {
         def result = me.knapik.GitHub.findPullRequests(env.JOB_NAME, env.BRANCH_NAME, env.GITHUB_ACCESS_TOKEN)
+        String githubAccessToken = env.GITHUB_ACCESS_TOKEN
 
         def pullRequestIds = result['pullRequestIds']
         def targetBranchRefs = result['targetBranchRefs']
 
         for (int i = 0; i < pullRequestIds.size(); ++i) {
-            cl(pullRequestIds[i], targetBranchRefs[i])
+            cl(pullRequestIds[i], targetBranchRefs[i], githubAccessToken)
         }
     }
 }
