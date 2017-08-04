b5b5441df395ffca6f6f4a230847575068fe61f8
Michał Knapik <michal.knapik@u2i.com>
2017-08-04 16:14:08 +0200

    Verbose docker compose project name

--- vars/withDockerCompose.groovy
+++ vars/withDockerCompose.groovy
@@ -1,11 +1,11 @@
 def call(String serviceName, Closure cl) {
-    def compose = new me.knapik.DockerCompose("${env.EXECUTOR_NUMBER}", serviceName, this)
+    def compose = new me.knapik.DockerCompose("${env.JOB_NAME}-${env.BUILD_NUMBER}-${env.EXECUTOR_NUMBER}", serviceName, this)
 
     withinDockerCompose(compose, cl)
 }
 
 def call(Closure cl) {
-    def compose = new me.knapik.DockerCompose("${env.EXECUTOR_NUMBER}", this)
+    def compose = new me.knapik.DockerCompose("${env.JOB_NAME}-${env.BUILD_NUMBER}-${env.EXECUTOR_NUMBER}", this)
 
     withinDockerCompose(compose, cl)
 }
